f1f3fe525e48fa16aa7ab416bc0b04ceb897b967
David <drdavialberto5@gmail.com>
2024-06-06 19:22:34 -0600

    Configuracion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,4 +50,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    //Se añade aparte.
+    implementation("com.google.firebase:firebase-database-ktx:20.0.0")
 }
