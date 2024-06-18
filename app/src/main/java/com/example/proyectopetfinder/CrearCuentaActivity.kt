88c30f0bb05cebbb91d3a3df8054c0d297acbdd5
SmashMex1369 <chris1203aless@gmail.com>
2024-06-18 08:20:18 -0600

    Crear cuentas y acceso a internet

--- app/src/main/java/com/example/proyectopetfinder/CrearCuentaActivity.kt
+++ app/src/main/java/com/example/proyectopetfinder/CrearCuentaActivity.kt
@@ -1,12 +1,19 @@
 package com.example.proyectopetfinder
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.proyectopetfinder.databinding.ActivityCrearCuentaBinding
+import com.example.proyectopetfinder.utilidades.Internet.tieneInternet
+import com.example.proyectopetfinder.utilidades.Internet.perdioConexion
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -24,6 +31,7 @@ class CrearCuentaActivity : AppCompatActivity() {
         val view = binding.root
         enableEdgeToEdge()
         setContentView(view)
+        habilitarCampos()
         database = Firebase.database.getReference("Usuarios")
         window.statusBarColor = ContextCompat.getColor(this,R.color.rojo)
         window.navigationBarColor = ContextCompat.getColor(this,R.color.rojo)
@@ -31,11 +39,17 @@ class CrearCuentaActivity : AppCompatActivity() {
         binding.btnRegistrar.setOnClickListener {
             
             if (validarCampos()){
-                val nombre=binding.etUsuario.text.toString()
-                val correo=binding.etCorreo.text.toString()
-                val contrasena=binding.etContraseA.text.toString()
-                val telefono=binding.etCelular.text.toString()
-                agrearUsuarioFirebase(nombre,correo,contrasena,telefono)
+                if (tieneInternet(this)){
+                    deshabilitarCampos()
+                    val nombre=binding.etUsuario.text.toString()
+                    val correo=binding.etCorreo.text.toString()
+                    val contrasena=binding.etContraseA.text.toString()
+                    val telefono=binding.etCelular.text.toString()
+                    agrearUsuarioFirebase(nombre,correo,contrasena,telefono)
+                }else{
+                    Toast.makeText(this, ContextCompat.getString(this,R.string.sin_conexion),Toast.LENGTH_LONG).show()
+                }
+
             }
 
         }
@@ -43,15 +57,39 @@ class CrearCuentaActivity : AppCompatActivity() {
     }
 
     fun agrearUsuarioFirebase(nombre:String, correo:String, contrasena:String, telefono:String){
-        var idUsuario = 0
-        database.child("IdUsuario").get().addOnSuccessListener {dataSnapshot ->
-            if(dataSnapshot.exists()){
-                idUsuario = dataSnapshot.getValue() as Int
+        if (tieneInternet(this)){
+            var idUsuario = 0
+            database.child("IdUsuario").get().addOnSuccessListener {dataSnapshot ->
+                Toast.makeText(this,"Registranse, espere un momento ...",Toast.LENGTH_LONG).show()
+                if(dataSnapshot.exists()){
+                    idUsuario = dataSnapshot.getValue().toString().toInt()
+                }
+                if (tieneInternet(this)){
+                    database.child("Usuario"+idUsuario).child("Nombre").setValue(nombre)
+                    database.child("Usuario"+idUsuario).child("Correo").setValue(correo)
+                    database.child("Usuario"+idUsuario).child("Contraseña").setValue(contrasena)
+                    database.child("Usuario"+idUsuario).child("Telefono").setValue(telefono)
+                    database.child("IdUsuario").setValue(idUsuario+1)
+                    limpiarCampos()
+                    Toast.makeText(this,"Cuenta registrada correctamente",Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else{
+                    perdioConexion(this)
+                    habilitarCampos()
+                }
+
+            }.addOnFailureListener { exception ->
+                Toast.makeText(this,"Error al crear la cuenta, intente nuavemente más tarde",Toast.LENGTH_LONG).show()
+
             }
+        }else{
+            perdioConexion(this)
         }
 
-    }
 
+    }
 
     fun validarCampos():Boolean{
         var valido = true
@@ -73,5 +111,26 @@ class CrearCuentaActivity : AppCompatActivity() {
         }
 
         return valido
+    }
+
+    fun deshabilitarCampos(){
+        binding.btnRegistrar.isEnabled=false
+        binding.etUsuario.isEnabled=false
+        binding.etCorreo.isEnabled=false
+        binding.etContraseA.isEnabled=false
+        binding.etCelular.isEnabled=false
+    }
+    fun habilitarCampos(){
+        binding.btnRegistrar.isEnabled=true
+        binding.etUsuario.isEnabled=true
+        binding.etCorreo.isEnabled=true
+        binding.etContraseA.isEnabled=true
+        binding.etCelular.isEnabled=true
+    }
+    fun limpiarCampos(){
+        binding.etUsuario.setText("")
+        binding.etCorreo.setText("")
+        binding.etContraseA.setText("")
+        binding.etCelular.setText("")
     }
 }
---
+++ app/src/main/java/com/example/proyectopetfinder/utilidades/Internet.kt
@@ -0,0 +1,24 @@
+package com.example.proyectopetfinder.utilidades
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.proyectopetfinder.R
+
+object Internet {
+    fun tieneInternet(context: Context):Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if(connectivityManager != null){
+            @Suppress("DEPRECATION")
+            val redActiva = connectivityManager.activeNetworkInfo
+            return redActiva !=null && redActiva.isConnected
+        }
+        return false
+    }
+
+    fun perdioConexion(context: Context){
+        Toast.makeText(context, ContextCompat.getString(context, R.string.sin_conexion), Toast.LENGTH_LONG).show()
+
+    }
+}
